1577566cdf468e527857b2a765d0c19d1ae52140
Sergio Barreras <sergiobg.isc@gmail.com>
2025-12-22 23:58:39 -0600

    Add author profiles for Bob Williams, Jane Doe, and John Smith
    - Created YAML files for new authors with their names and avatar paths.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
@@ -9,12 +9,22 @@ const blogCollection = defineCollection({
       description: z.string(),
       image: image(),
       // Relacion
-      author: z.string(),
+      author: reference("author"),
       // Relacion
       tags: z.array(z.string()),
     }),
 });
 
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
+});
+
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
